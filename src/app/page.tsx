81ebb4558531b7d5bbf80a01c5affc365c0e748d
Tobias Zethoven <tzethoven@gmail.com>
2025-07-16 19:53:14 +0200

    setup prisma

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,107 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaClient } from "../../generated/prisma";
 
-export default function Home() {
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+const globalForPrisma = global as unknown as { prisma: PrismaClient };
+const adapter = new PrismaPg({
+	connectionString: process.env.POSTGRES_PRISMA_URL,
+});
+const prisma: PrismaClient =
+	globalForPrisma.prisma || new PrismaClient({ adapter });
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+
+export default async function Home() {
+	await prisma.user.findUnique({ where: { id: "123" } });
+	return (
+		<div className={styles.page}>
+			<main className={styles.main}>
+				<Image
+					className={styles.logo}
+					src="/next.svg"
+					alt="Next.js logo"
+					width={180}
+					height={38}
+					priority
+				/>
+				<ol>
+					<li>
+						Get started by editing <code>src/app/page.tsx</code>.
+					</li>
+					<li>Save and see your changes instantly.</li>
+				</ol>
+
+				<div className={styles.ctas}>
+					<a
+						className={styles.primary}
+						href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+						target="_blank"
+						rel="noopener noreferrer"
+					>
+						<Image
+							className={styles.logo}
+							src="/vercel.svg"
+							alt="Vercel logomark"
+							width={20}
+							height={20}
+						/>
+						Deploy now
+					</a>
+					<a
+						href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+						target="_blank"
+						rel="noopener noreferrer"
+						className={styles.secondary}
+					>
+						Read our docs
+					</a>
+				</div>
+			</main>
+			<footer className={styles.footer}>
+				<a
+					href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					<Image
+						aria-hidden
+						src="/file.svg"
+						alt="File icon"
+						width={16}
+						height={16}
+					/>
+					Learn
+				</a>
+				<a
+					href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					<Image
+						aria-hidden
+						src="/window.svg"
+						alt="Window icon"
+						width={16}
+						height={16}
+					/>
+					Examples
+				</a>
+				<a
+					href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					<Image
+						aria-hidden
+						src="/globe.svg"
+						alt="Globe icon"
+						width={16}
+						height={16}
+					/>
+					Go to nextjs.org →
+				</a>
+			</footer>
+		</div>
+	);
 }
